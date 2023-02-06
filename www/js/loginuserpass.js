ed40f941ae02f1dee170f3eeca8fa4bfbe02c702
Chris Coleman <chrisc@vmunix.com>
2023-02-06 10:37:12 -0500

    Moved www to public

--- www/js/loginuserpass.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function ($) {
-    $('#submit_button').on('click', function () {
-        $(this).attr('disabled', 'disabled');
-        $(this).html($(this).data('processing'));
-        $(this).parents('form').submit();
-    });
-});
